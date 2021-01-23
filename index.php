b99ea23d628ce106a386aa91dfebe9a6fcffb443
mendi <mendi_barel@hotmail.com>
2021-01-23 18:53:06 +0200

    fix bg

--- index.php
+++ index.php
@@ -5,29 +5,29 @@
 	<meta charset="UTF-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<!--link rel="stylesheet" href="style.css"-->
-	<style>
-		* { padding:0; margin:0; box-sizing: border-box; }
-		body { display:flex; align-items:center; flex-direction: column; font-family:"Raleway", sans-serif; background:#1edcdc;}
-		
-		body {background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);background-size: 400% 400%; animation: gradient 15s ease infinite;}
-		@keyframes gradient {0%{background-position: 0% 50%;} 50%{background-position: 100% 50%;} 100%{background-position: 0% 50%;}}
-		h1 { margin-top:33px; padding:24px; text-transform: uppercase; font-weight: 300; font-size: 32px; letter-spacing: 2px; color:#a15151;}
-		h1::after { content:""; display:flex; border-top: 2px solid #585858; margin-top: 8px;}
-		#inputAddRow { height:48px; width:600px;background:#fff; border:none; padding: 8px 18px; font-family:"Raleway", sans-serif; font-size:32px; }
+	<style> 
+		* {  width:80vw; padding:0; margin:0 auto;  box-sizing: border-box; }
+		body { font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif; }
+		body { background: linear-gradient( 45deg, #1e7752, #e73c7e, #23a6d5, #23d5ab); background-attachment: fixed; background-size: 400% 400%; animation: AnimationName 10s ease infinite;}
+		@keyframes AnimationName { 0%{background-position:0% 50%}  50%{background-position:100% 50%}  100%{background-position:0% 50%} }
+
+		h1 { margin-top:2vh; padding:0; text-align:center; text-transform: uppercase;  font-weight: 300; font-size:5vw; letter-spacing: 0.3vw; color:#a1215170; }
+		h1::after { content:""; display:flex; border-top: 2px solid #585858; margin-top: .2em;}
+		#inputAddRow { height:48px; background:#bfb5; border:none; padding: 8px 18px; font-family:"Raleway", sans-serif; font-size:32px; }
 		#inputAddRow:focus {outline:none;}
-		#buttonAddRow {background-color:#aaaaaaaa; height:64px; width:600px;font-size:32px; display:inline-block; padding:0.35em 1.2em; border:0.1em solid #FFFFFF; margin:0 0.3em 0.3em 0; 
-							border-radius:0.12em;box-sizing: border-box;text-decoration:none; font-family:'Roboto',sans-serif; font-weight:300;color:#000;text-align:center; transition: all 0.5s;}		
-		#buttonAddRow:hover {color:#000000;background-color:#FFFFFF;}
-		#searchBox { height:48px; width:600px;background:#fff; border:none; padding: 8px 18px; font-family:"Raleway", sans-serif; font-size:32px; }
+		#buttonAddRow {background-color:#aaaaaaaa; height:64px; font-size:32px; display:inline-block; border:0.1em solid #FFFFFF; margin:2px auto 10px auto;
+							border-radius:5px;  font-family:'Roboto',sans-serif; font-weight:300; color:#000; text-align:center; transition: all 0.5s;}		
+		#buttonAddRow:hover {color:#f00;background-color:#527;}
+		#searchBox { height:48px; background:#fff; border:none; padding: 8px 18px; font-family:"Raleway", sans-serif; font-size:32px; }
 		#searchBox:focus {outline:none;}
-		#searchResult {padding:32px; list-style:none; background:#555; width:600px; color:#fff; height:400px; overflow-y:scroll;}
+		#searchResult {padding:32px; list-style:none; background:#555; color:#fff; height:400px; overflow-y:scroll;}
 		#searchResult li {padding:8px 0px; border-left: 1px solid #645f5f; padding-left: 24px;}
 		#searchResult::-webkit-scrollbar { width:8px; background:#111;}
 		#searchResult::-webkit-scrollbar-thumb { width:8px; background:#880;}
 		table {font-family: arial, sans-serif; border-collapse: collapse; width: 100%;}
 		td, th {border: 1px solid #dddddd; text-align: left; padding: 8px;}
 		tr:nth-child(even) {background-color: #dddddd;}
-		
+		*/
 	</style>
 	<title>Live</title>
 </head>
