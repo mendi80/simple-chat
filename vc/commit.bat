4c0b82bb0b5c13652231c87cd4708ec3ac7af5b2
mendi6 <mendi_barel@hotmail.com>
2021-04-03 17:32:22 +0300

    ip config

--- vc/commit.bat
+++ vc/commit.bat
@@ -5,11 +5,13 @@ SET "tmpdir=%DRV%\temp\website"
 
 REM cd A:\temp\website  OR git clone https://github.com/vcorona-israel/website.git
 REM rm -rf .git && git init -b main && git commit -v --allow-empty -m "root" 
-REM git add -v --all -- :!index.html && git commit -vm "base" && git add . && git commit -vm "html"
-REM git remote add origin https://github.com/vcorona-israel/website.git && git push -u --force origin main
+REM git add -v --all -- ':!index.html' && git commit -vm "base" && git add . && git commit -vm "html"
+REM git remote add origin git@siders:vcorona-israel/website.git && git push -u --force origin main
 
 copy /Y %DRV%\xampp\htdocs\vc\website\index.html %tmpdir% || goto :error
 cd /D %tmpdir% || goto :error
+git pull || goto :error
+REM git add "index.html" || goto :error
 git status || goto :error
 git commit --all --amend --no-edit || goto :error
 git push origin main --force || goto :error
@@ -20,5 +22,6 @@ echo Done!
 exit /b DONE!
 
 :error
+cd %~dp0
 echo Failed with error #%errorlevel%.
 exit /b %errorlevel%
